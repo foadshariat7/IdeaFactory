87f3f9c7e028328555f031bc7bcc2347de4d7d3d
Kuro1Fury <skycoder0820@gmail.com>
2023-04-25 02:13:04 -0700

    still fail

--- idea-factory/src/App.jsx
+++ idea-factory/src/App.jsx
@@ -7,15 +7,15 @@ import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
+    // <AuthProvider>
+    //   <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+    //     <div className="w-100" style={{ maxWidth: "400px" }}>
+    //       <Signup />
+    //     </div>
+    //   </Container>
 
-    </AuthProvider>
-
+    // </AuthProvider>
+    <Signup />
   )
 }
 
--- idea-factory/src/firebase.jsx
+++ idea-factory/src/firebase.jsx
@@ -3,14 +3,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const app = initializeApp({
+
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+}
 
-export const auth = getAuth()
+const app = initializeApp(firebaseConfig);
+
+export const auth = app.auth()
 export default app
