e69808181fa2de5e4ead9d2bd13625796923f063
Kuro1Fury <skycoder0820@gmail.com>
2023-04-25 01:39:49 -0700

    fail?

---
+++ idea-factory/src/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const UserProfile = () => {
+    return (
+        <div className="container">
+
+
+        </div>
+    )
+}
+
+export default UserProfile
