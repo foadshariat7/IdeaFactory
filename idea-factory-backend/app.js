ee4f34f30b55a73dac51616ed5e2a4bec6d35ad3
Caleb Huang <calebhuangsea@gmail.com>
2023-04-25 21:19:29 -0700

    database connection sample and app

--- idea-factory-backend/app.js
+++ idea-factory-backend/app.js
@@ -1,20 +1,26 @@
 const express = require('express')
-const { connectToDb, getDb } = require('./db/db')
+const { connectToDb, getDb, closeDb } = require('./db/db')
 
-// init app & middleware
+// initialize app
 const app = express()
 
+// listening port 3000
+app.listen(3000, () => {
+    console.log('app listening on port 3000')
+})
+
+// database object
 let db
 
-//db connection
-connectToDb((err) => {
-    if (!err) {
-        app.listen(3000, () => {
-            console.log('app listening on port 3000')
-        })
-        db = getDb()
-    }
-})
+//db connection function (use as middle ware for API that requires database access)
+function connect(req, res, next) {
+    connectToDb((err) => {
+        if (!err) {
+            db = getDb()
+            next()
+        }
+    });
+}
 
 // sample route
 app.get('/', (req, res) => {
@@ -22,16 +28,15 @@ app.get('/', (req, res) => {
 });
 
 // sample route to fetch stuff from user collection
-app.get('/users', (req, res) => {
+app.get('/users', connect, (req, res) => {
     let result = []
-
     db.collection('Users')
         .find()
         .forEach(user => result.push(user))
         .then(() => {
             console.log('successful')
             res.status(200).json(result)
-        })
+        }).then(closeDb)
         .catch(() => {
             console.log('err')
             res.status(500).json({err: '123'})
--- idea-factory-backend/db/db.js
+++ idea-factory-backend/db/db.js
@@ -1,21 +1,39 @@
 const { MongoClient } = require('mongodb')
+const {DBNAME, DBUSER, DBPASSWORD} = require('./config')
 
 let dbConnection
 // username & password: ideafactory
-let uri = 'mongodb+srv://ideafactory:ideafactory@cluster0.y6ysia6.mongodb.net/IdeaFactory?retryWrites=true&w=majority'
-
+let uri = `mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0.y6ysia6.mongodb.net/${DBNAME}?retryWrites=true&w=majority`
 
 module.exports = {
+    // connect to database
     connectToDb: (cb) => {
-        MongoClient.connect(uri)
-            .then((client) => {
-                dbConnection = client.db()
-                return cb()
-            })
-            .catch(err => {
-                console.log(err)
-                return cb(err)
-            })
+      MongoClient.connect(uri)
+        .then((dbClient) => {
+          console.log('Connected to database')
+          client = dbClient
+          return cb()
+        })
+        .catch((err) => {
+          console.log('Error connecting to database', err)
+          return cb(err)
+        })
     },
-    getDb: () => dbConnection
-}
+    //get database object
+    getDb: () => client.db(),
+    //close database connection
+    closeDb: () => {
+      if (client) {
+        client.close()
+          .then(() => console.log('Connection to database closed'))
+          .catch((err) => console.log('Error closing connection to database', err))
+      }
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
